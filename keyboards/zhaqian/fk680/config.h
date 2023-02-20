93b8d5ae3ee35c7580ec6d49fd83592079bae7ec
MattBoog <arcodeboer@gmail.com>
2023-02-20 11:28:33 +0100

    Changed vendorID
    To make sure it's compatible with newer VIA versions the VendorID had to be altered from 0xFEED to a value over 0x4000.

--- keyboards/zhaqian/fk680/config.h
+++ keyboards/zhaqian/fk680/config.h
@@ -18,7 +18,7 @@
 
 #include "config_common.h"
 
-#define VENDOR_ID       0xFEED
+#define VENDOR_ID       0x8657
 #define PRODUCT_ID      0x6060
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    ZhaQian
